1e5395c551ed7fe9120b892c121b1299eed881e7
Emil Ekblom <ekblomemil@gmail.com>
2021-01-20 09:37:56 +0100

    added IV surface computation

--- Sidetracks/yahooDailyGamma.py
+++ Sidetracks/yahooDailyGamma.py
@@ -8,7 +8,10 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 import datetime
+import Backtest as bt
+import matplotlib.pyplot as plt
 from scipy.stats import norm
+import sys
 np.seterr(divide='ignore', invalid='ignore')
 
 #Get Option Chain From Yahoo Finance
@@ -38,12 +41,12 @@ def getOptionChain(symbol):
     tbill = yf.Ticker("^IRX") #Get risk free rate (US 13-week T-bill)
     rf    = tbill.info["previousClose"] / 100
     
-    cp_flag = 1*options["CALL"].to_numpy()
-    K       = options["strike"].to_numpy()
-    S       = tk.info["previousClose"]
+    cp_flag   = 1*options["CALL"].to_numpy()
+    K         = options["strike"].to_numpy()
+    S         = tk.info["previousClose"]
     divYield  = tk.info["yield"]
-    IV      = options["impliedVolatility"].to_numpy()
-    T       = options["dte"].to_numpy()
+    IV        = options["impliedVolatility"].to_numpy()
+    T         = options["dte"].to_numpy()
     
     d1 = (np.log(S/K) + (rf + IV**2/2)*T) / (IV*np.sqrt(T))
     
@@ -59,6 +62,9 @@ def getOptionChain(symbol):
     options[['bid', 'ask', 'strike']] = options[['bid', 'ask', 'strike']].apply(pd.to_numeric)
     options['mark'] = (options['bid'] + options['ask']) / 2 # Calculate the midpoint of the bid-ask
     
+    spotDummy = np.ones((len(options), 1))*S
+    options["spot"] = spotDummy
+                        
     # Drop unnecessary and meaningless columns
     options = options.drop(columns = ['contractSize', 'currency', 'change', 'percentChange', 'lastTradeDate', 'lastPrice'])
 
@@ -86,8 +92,60 @@ def computeNetGammaExposure(optionChain):
     return netGamma
 
 yahoo_ticker = "SPY"
-chain        = getOptionChain(yahoo_ticker)
-netGamma     = computeNetGammaExposure(chain)
+options      = getOptionChain(yahoo_ticker)
+#netGamma     = computeNetGammaExposure(options)
 today        = datetime.date.today()
 
-print("Market Maker Net Gamma Before Open", today, ":",  netGamma)
+#print("Market Maker Net Gamma Before Open", today, ":",  netGamma)
+
+
+#Plot IV surface
+expDates       = options["expirationDate"]
+expDatesNum    = bt.yyyymmdd(expDates).reshape(len(expDates,))
+uniqueExpDates = np.unique(expDatesNum)
+nExpirations   = np.size(uniqueExpDates)
+OTMFlag        = options["inTheMoney"].to_numpy() == 0
+cp_flag        = options["CALL"].to_numpy()
+ImpliedVol     = options["impliedVolatility"].to_numpy()
+spot           = options["spot"].to_numpy()
+strike         = options["strike"].to_numpy()
+
+#Vsurface = []
+
+matName = np.array(["Front Month", "First Back Month", "Second Back Month"])
+colors  = np.array(["blue", "red", "black"])
+for i in np.arange(0, 3):
+    exp        = uniqueExpDates[i]
+    isRightExp = (exp == expDatesNum)
+    
+    rel_OTM    = OTMFlag[isRightExp]
+    rel_IV     = ImpliedVol[isRightExp]
+    rel_spot   = spot[isRightExp]
+    rel_strike = strike[isRightExp]
+    moneyness  = rel_strike / rel_spot    
+    
+    #Keep Only OTM Values
+    moneyness_OTM = moneyness[rel_OTM]
+    IV_OTM         = rel_IV[rel_OTM]
+    nOTMOptions   = np.size(moneyness_OTM)
+    
+    IVmat   = np.concatenate((IV_OTM.reshape(nOTMOptions, 1), moneyness_OTM.reshape(nOTMOptions, 1)), axis = 1)
+    IVdf    = pd.DataFrame.from_records(IVmat, columns = ["ImpliedVol", "Moneyness"])
+    sortDf  = IVdf.sort_values("Moneyness")
+    sortMat = sortDf.to_numpy()
+    
+    plt.plot(sortMat[:, 1], sortMat[:, 0], color = colors[i], label = matName[i])   
+    plt.title("Implied Volatility - No Smoothing")
+    plt.xlabel("Moneyness (K/S)")
+    plt.ylabel("IV (%)")
+    plt.legend()
+    
+
+
+
+
+
+
+
+
+
