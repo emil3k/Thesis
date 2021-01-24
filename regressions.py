f97b27b56a56c91744dbc98a1f854a92b1a078a8
Emil Ekblom <ekblomemil@gmail.com>
2021-01-24 16:11:15 +0100

    added convex term, and scatter plots

--- regressions.py
+++ regressions.py
@@ -27,7 +27,7 @@ for ticker in UnderlyingTicker:
     AggregateData.append(data)
 
 
-#Compute returns
+#Compute returns and adjusted gamma
 nAssets = len(UnderlyingTicker)
 TotalReturns = []
 XsReturns    = []
@@ -36,8 +36,8 @@ for i in np.arange(0, nAssets):
     ticker = UnderlyingTicker[i]
     
     if ticker  != "VIX":
-       price  = data[ticker].to_numpy()
-       rf     = data["Rf Daily"].to_numpy() #grab Rf    
+       price    = data[ticker].to_numpy()
+       rf       = data["Rf Daily"].to_numpy() #grab Rf    
        
        #Compute returns 
        ret   = price[1:] / price[0:-1] - 1 
@@ -59,8 +59,8 @@ for i in np.arange(0, nAssets):
        #Store
        TotalReturns.append(frontTotalReturns)
        XsReturns.append(frontXsReturns)
-       
-       
+    
+     
 def plotResiduals(residuals, lag = None, ticker = None, color = '#0504aa', histtype = 'stepfilled'):
     skewness   = skew(residuals)
     xsKurtosis = kurtosis(residuals) - 3
@@ -93,45 +93,94 @@ lagList = [1, 2, 5, 10]
 #lagList = [1]
 nRegs   = len(lagList)
 hist    = True
+scatter = True
+RegResultList = []
 
-
-for j in np.arange(0, 1):
+for j in np.arange(0, len(UnderlyingTicker)):
     ticker   = UnderlyingTicker[j]
     data     = AggregateData[j]
     xsret    = XsReturns[j]
     netGamma = data["netGamma"].to_numpy()
+    netGamma_norm = (netGamma - np.mean(netGamma)) / np.std(netGamma)
+    
+    MAdollarVolume = data["MADollarVolume"].to_numpy() 
+    adjNetGamma    = netGamma / MAdollarVolume 
+    adjNetGamma_norm = (adjNetGamma - np.mean(adjNetGamma)) / np.std(adjNetGamma)
     
     regResults = []
     
     for i in np.arange(0, nRegs):
-        lag = lagList[i]
-        y   = np.abs(xsret[lag:])
-        X   = netGamma[0:-lag]
-        X   = sm.add_constant(X)
+        lag    = lagList[i]
+        y      = np.abs(xsret[lag:])
+        nObs   = np.size(y)
+        
+        X_raw  = netGamma_norm[0:-lag]
+        X_sqrd = netGamma_norm[0:-lag]**2
+        X      = np.concatenate((X_raw.reshape(nObs, 1), X_sqrd.reshape(nObs, 1)), axis = 1)
+        X      = sm.add_constant(X)
     
         reg       = sm.OLS(y, X).fit()
-        coefs     = np.round(reg.params, decimals = 3)
-        tvals     = np.round(reg.tvalues, decimals = 3)
-        pvals     = np.round(reg.pvalues, decimals = 3)
-        r_squared = np.round(reg.rsquared, decimals = 3)
+        coefs     = np.round(reg.params, decimals = 5)
+        tvals     = np.round(reg.tvalues, decimals = 5)
+        pvals     = np.round(reg.pvalues, decimals = 5)
+        r_squared = np.round(reg.rsquared, decimals = 5)
         
-        legend  = np.array(["coefficient", "t stats", "p-values", "r_squared"])
-        alpha   = np.array([coefs[0], tvals[0], pvals[0], r_squared])
-        beta    = np.array([coefs[1], tvals[1], pvals[1], " "])
+        legend  = np.array(["statistic", "coefficient", "t stats", "p-values", "r_squared"])
+        alpha   = np.array(["alpha", coefs[0], tvals[0], pvals[0], r_squared])
+        b1      = np.array(["b1", coefs[1], tvals[1], pvals[1], " "])
+        b2      = np.array(["b1", coefs[2], tvals[2], pvals[2], " "])
         
-        reg_df = pd.DataFrame()
-        reg_df["Statistic"] = legend
-        reg_df["alpha"] = alpha
-        reg_df["beta"] = beta
+        lag_df = pd.DataFrame()
         
-        regResults.append(reg_df)
+        if i == 0:
+            lag_df["Statistic(Lag = " + str(lag) + ")"] = legend
+            lag_df["alpha (Lag = " + str(lag) + ")"]  = alpha
+            lag_df["B1 (Lag = " + str(lag) + ")"] = b1
+            lag_df["B2 (Lag = " + str(lag) + ")"] = b2 
+        else:
+            lag_df["alpha (Lag = " + str(lag) + ")"] = alpha
+            lag_df["B1 (Lag = " + str(lag) + ")"] = b1 
+            lag_df["B2 (Lag = " + str(lag) + ")"] = b2 
         
-        print(reg_df)
+        regResults.append(lag_df)
         
+        if scatter == True:
+            x        = np.linspace(np.min(X_raw), np.max(X_raw), np.size(X_raw))
+            reg_line = coefs[0] + coefs[1] * x + coefs[2]*x**2
+            
+            fig, ax = plt.subplots()
+            
+            ax.scatter(netGamma_norm[0:-lag], y, color = '#0504aa', s = 0.5)
+            ax.plot(x, reg_line, color = "red", alpha = 0.5)
+            fig.suptitle("Absolute Returns vs Net Gamma Exposure (normalized)")
+            ax.set_xlabel("Net Gamma Exposure (Normalized)")
+            ax.set_ylabel("Daily Absolute Returns")
+            
+            textstr = '\n'.join((
+                'Asset = ' + ticker,
+                'Lag = ' + str(lag) + ' days',
+                r'$\alpha=%.5f$' % (coefs[0], ),
+                r'$\beta_1=%.5f$' % (coefs[1], ),
+                r'$\beta_2=%.5f$' % (coefs[2], ),
+                ))
+
+            # these are matplotlib.patch.Patch properties
+            props = dict(boxstyle='round', facecolor=prefColor, alpha=0.2)
+
+            # place a text box in upper left in axes coords
+            ax.text(0.65, 0.95, textstr, transform=ax.transAxes, fontsize=14, verticalalignment='top', bbox=props)
+            plt.show() 
+            
+            
         residuals  = reg.resid
         if hist == True: #Plot netGamma histogram
             plot = plotResiduals(residuals, lag = lag, ticker = ticker)
     
+    #Concatenate
+    AssetDf = pd.concat(regResults, axis = 1)
+    print(AssetDf)
+    
+    RegResultList.append(AssetDf)
     
     
 # X   = netGamma[0:-lag]
