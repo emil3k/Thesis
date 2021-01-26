eda748419839633b9c4869040fcf8f468a5535f1
Emil Ekblom <ekblomemil@gmail.com>
2021-01-26 21:09:09 +0100

    daily atm prices for calls and puts and back and front month

--- tradingstrategy.py
+++ tradingstrategy.py
@@ -44,13 +44,22 @@ netGamma = AggregateData["netGamma"].to_numpy()
 
 OptionCols      = OptionDataTr.columns
 OptionDataArr   = OptionDataTr.to_numpy()
-FrontATMCalls = np.zeros((1, np.size(OptionCols)))
-FrontATMPuts  = np.zeros((1, np.size(OptionCols)))
 
-nDays           = np.size(UniqueDates)
 currentlyInvested = 0 #initialize no position
-putPortfolio = np.zeros((nDays, ))
-error = []
+
+frontError   = []
+backError    = []
+
+#Set params for loop
+nDays           = np.size(UniqueDates)
+rollDay  = 7
+rollover = np.zeros((nDays, 1))
+frontATMCalls = np.zeros((nDays, np.size(OptionCols)))
+frontATMPuts  = np.zeros((nDays, np.size(OptionCols)))
+backATMCalls   = np.zeros((nDays, np.size(OptionCols)))
+backATMPuts    = np.zeros((nDays, np.size(OptionCols)))
+
+#Grab front and back month ATM Options
 for i in np.arange(0, nDays):
             
     day = UniqueDates[i] #grab day
@@ -61,65 +70,88 @@ for i in np.arange(0, nDays):
     frontExpiry    = np.unique(expirations)[0] #grab front month expiration
     backExpiry     = np.unique(expirations)[1] #grab back month expiration
     daysToMatFront = frontExpiry - day #days to maturity of front contract
+ 
     
-    #Set expiration for options strategy will trade
-    if daysToMatFront > 7:
-        useFrontMonth = True
-        exdate = frontExpiry 
-    else:
-        useFrontMonth = False
-        exdate = backExpiry
-    
-    isRightExp = (expirations == exdate)
-    isATM      = (rightDayOptions[:, -1] == 1)
-
-    optionsToGet = rightDayOptions[isRightExp * isATM, :]    
-    
-    if np.size(optionsToGet, 0) < 2 and i > 0:
-        #raise ValueError("Not enough ATM options to create straddle")
-        error.append(i)
-        FrontATMCalls = np.concatenate((FrontATMCalls, CallsToGet), axis = 0)
-        FrontATMPuts  = np.concatenate((FrontATMPuts, PutsToGet), axis = 0)   
-        continue
+    if daysToMatFront < rollDay:
+        rollover[i] = 1
         
-    if np.size(optionsToGet, 0) > 2: #grab the options closest to ATM
-       strikes  = optionsToGet[:, 3] #grab strikes
-       spot     = optionsToGet[:, 19][0] #grab spot
-       toGetIdx = ((strikes - spot) == np.min(strikes - spot)) #index of options closest to spot
-       toGet    = optionsToGet[toGetIdx, :]
-       CallsToGet = toGet[(toGet[:,2] == 1), :]
-       PutsToGet  = toGet[(toGet[:,2] == 0), :]
     
-    FrontATMCalls = np.concatenate((FrontATMCalls, CallsToGet), axis = 0)
-    FrontATMPuts  = np.concatenate((FrontATMPuts, PutsToGet), axis = 0)   
+    isFrontExp = (expirations == frontExpiry)
+    isBackExp  = (expirations == backExpiry)
+    isATM      = (rightDayOptions[:, -1] == 1)
     
-   
-   #callstrikes = optionsToGet[:, 3][optionsToGet[:, 2] == 1]
-   #putstrikes  = optionsToGet[:, 3][optionsToGet[:, 2] == 0]
-   #callToGet = ((callstrikes - spot) ==  np.min(callstrikes - spot))
-   #puyToGet = ((putstrikes - spot) ==  np.min(putstrikes - spot))
+    FrontToGet = rightDayOptions[isFrontExp * isATM, :]    
+    BackToGet  = rightDayOptions[isBackExp * isATM, :]
+    
+    #Check number of calls and puts
+    nCallsFront = np.sum(FrontToGet[:, 2] == 1)
+    nPutsFront  = np.sum(FrontToGet[:, 2] == 0)
+    nCallsBack  = np.sum(BackToGet[:, 2] == 1)
+    nPutsBack   = np.sum(BackToGet[:, 2] == 0)
+    
+    
+    def getATMOptions(options):
+        strikes    = options[:, 3]      #grab strikes
+        spot       = options[:, 19][0]  #grab spot
+        cpflag     = options[:, 2]      #grab call/put flag
+        
+        callstrikes = strikes[cpflag == 1]
+        calloptions = options[cpflag == 1, :]
+        callsToGetIdx   = ((callstrikes - spot) == np.min(callstrikes - spot))
+        
+        putstrikes = strikes[cpflag == 0]
+        putoptions = options[cpflag == 0, :]
+        putsToGetIdx   = ((putstrikes - spot) == np.min(putstrikes - spot))
+        
+        ATMCalls = calloptions[callsToGetIdx, :]
+        ATMPuts  = putoptions[putsToGetIdx, :]
+        
+        #ATMOptions = options[toGetIdx, :] #grab the options closest
+        #ATMCalls   = ATMOptions[(cpflag[toGetIdx] == 1), :] #separate calls
+        #ATMPuts    = ATMOptions[(cpflag[toGetIdx] == 0), :] #separate puts
+        
+        return ATMCalls, ATMPuts
+    
+    
+    
+    ### NEED TO GET PREVIOUS DAY PRICE TO COMPUTE RETURNS
+    
+    #Grab Front ATM Options
+    if np.size(FrontToGet, 0) >= 2: #grab front
+        frontCallsToGet, frontPutsToGet = getATMOptions(FrontToGet)
+        frontATMCalls[i, :] = frontCallsToGet
+        frontATMPuts[i, :]  = frontPutsToGet
+    
+    else: #not enough ATM options, use previous day options and  save error for investigation
+        frontError.append(i)
+        frontATMCalls[i, :] = frontATMCalls[i-1, :] #fill with previous
+        frontATMPuts[i, :]  = frontATMPuts[i-1, :] #fill with previous
+        
+    #Grab Back ATM Options
+    if np.size(BackToGet, 0) >= 2: #grab back
+        backCallsToGet, backPutsToGet = getATMOptions(BackToGet)
+        backATMCalls[i, :] = backCallsToGet
+        backATMPuts[i, :]  = backPutsToGet
+    
+    else:
+        backError.append(i)
+        backATMCalls[i, :]  = backATMCalls[i-1, :] #fill with previous
+        backATMCalls[i, :]  = backATMCalls[i-1, :] #fill with previous 
 
-#What do with more options
-
-#cp_flag = rightDayOptions[:, 2]
-#rightDayCalls = rightDayOptions[(cp_flag == 1), :] #grab calls
-#rightDayPuts  = rightDayOptions[(cp_flag == 0), :] #grab puts
-
-   
-     
 
 
+## Error investigation
+
+errorDate = UniqueDates[frontError[26]]
+isErrorDate = (OptionDates == errorDate)
+errorDayOptions = OptionDataArr[isErrorDate, :]
+errorDayOptions = errorDayOptions[:, [0, 1, 2, 3, 19, -1]]
 
 
+back = errorDayOptions[:, 1] == errorDate
+atm  = errorDayOptions[:, -1] == 1
 
-
-
-
-
-
-
-
-
+tt  = errorDayOptions[back*atm, :]
 
 
 
