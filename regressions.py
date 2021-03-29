d56a970f9dc2811d9575bd8cb79ba0c889438faf
Emil Ekblom <ekblomemil@gmail.com>
2021-03-29 17:39:25 +0200

    cleaned up a bit, added liquidity regression

--- regressions.py
+++ regressions.py
@@ -19,9 +19,9 @@ from sklearn.linear_model import LassoCV
 UnderlyingAssetName   = ["SPX Index", "SPY US Equity", "NDX Index", "QQQ US Equity", "RUT Index", "IWM US Equity", "VIX Index"]
 UnderlyingTicker      = ["SPX", "SPY", "NDX", "QQQ", "RUT", "IWM", "VIX"]
 IsEquityIndex        = [True, False, True, False, True, False, False]
-#UnderlyingAssetName   = ["SPY US Equity"]
-#UnderlyingTicker      = ["SPY"]
-#IsEquityIndex         = [False]
+#UnderlyingAssetName   = ["SPX US Equity"]
+#UnderlyingTicker      = ["SPX"]
+#IsEquityIndex         = [True]
 
 loadloc               = "C:/Users/ekblo/Documents/MScQF/Masters Thesis/Data/AggregateData/"
 prefColor             = '#0504aa'
@@ -114,11 +114,9 @@ def plotResiduals(residuals, lag = None, ticker = None, color = '#0504aa', histt
 
 # #Set up Regressions
 #lagList = [1, 2, 5, 10]
-lagList = [1]
-nRegs   = len(lagList)
+lag     = 1
 hist    = False
 scatter = False
-RegResultList = []
 
 for j in np.arange(0, len(UnderlyingTicker)):
     ticker   = UnderlyingTicker[j]
@@ -131,7 +129,12 @@ for j in np.arange(0, len(UnderlyingTicker)):
     netGamma       = data["netGamma"].to_numpy()
     marketCap      = data["Market Cap"].to_numpy()
     MAdollarVolume = data["MADollarVolume"].to_numpy() 
-    
+    ILLIQ          = data["ILLIQ"].to_numpy()
+    ILLIQMedian    = np.median(ILLIQ)    
+    ILLIQMean      = np.mean(ILLIQ)  
+    #plt.plot(ILLIQ)
+    #plt.plot(np.ones((len(ILLIQ),))*ILLIQMedian, "--r")
+    #plt.plot(np.ones((len(ILLIQ),))*ILLIQMean, "--b")
     
     #Net Gamma Measures
     netGamma_norm   = (netGamma - np.mean(netGamma)) / np.std(netGamma)
@@ -170,181 +173,223 @@ for j in np.arange(0, len(UnderlyingTicker)):
     #Feature correlation
     IndependentVarDf = pd.DataFrame.from_records(X_control, columns = [r'$netGamma_t%.5f$', r'$IVOL_t%.5f$', r'$IVOL_{t-1}%.5f$', r'$IVOL_{t-2}%.5f$', r'$|R_t|%.5f$', r'$|R_{t-1}|%.5f$', r'$|R_{t-2}|%.5f$' ])
     corrMatrix       = IndependentVarDf.corr()
+    
+    plt.figure()
     sn.heatmap(corrMatrix, annot = True)
-
+    plt.title("Independent Variable Correlation, " + ticker)
     
+    ######## Illiquidity regression ###############
+    
+    negNetGammaDummy   = (netGamma_scaled < 0)
+    IlliquidDummy      = (ILLIQ > ILLIQMedian)
+    InteractionDummy   = IlliquidDummy * negNetGammaDummy
+    
+    X_liq = np.concatenate((negNetGammaDummy.reshape(-1,1), IlliquidDummy.reshape(-1,1), InteractionDummy.reshape(-1,1)), axis = 1)
+    
+    
+    
+    ####################################################
+    ######### Fit regressions and store result #########
+    
+    #######################################
+    ######### Standard Regression #########    
+    y      = np.abs(xsret[lag:])
+    nObs   = np.size(y)
    
-    regResults = []
+    X      = X[0:-lag, :]       #Lag matrix accordingly 
+    X      = sm.add_constant(X) #add constant
+
+    reg       = sm.OLS(y, X).fit()
+    coefs     = np.round(reg.params*100, decimals = 3) #Multiply coefs by 100 to get in bps format
+    tvals     = np.round(reg.tvalues, decimals = 3)
+    pvals     = np.round(reg.pvalues, decimals = 3)
+    r_squared = np.round(reg.rsquared, decimals = 3)
+        
+         
+    #LASSO FOR Parameter selection
+    LassoFeature = LassoCV(n_alphas = 100, fit_intercept = False, normalize = False, cv = 10, \
+                          max_iter = 10000, tol = 1e-3)
+    LassoFeature.fit(X, y)
+    LassoCoefs = LassoFeature.coef_
+    isImportantFeatures = (LassoCoefs != 0) 
+        
+
+    ### Result Print
+    legend = np.array(['$netGamma_{t - ' + str(lag) + '}$', " ", '$IVOL_{t-1}$', " ", 'Intercept', " ", '$R^2$' ])
     
-    for i in np.arange(0, nRegs):
-        lag    = lagList[i]
-        y      = np.abs(xsret[lag:])
-        nObs   = np.size(y)
+    sign_test = []
+    for pval in pvals:
+        if pval < 0.01:
+            sign_test.append("***")
+        elif pval < 0.05:
+            sign_test.append("**")
+        elif pval < 0.1:
+            sign_test.append("*")
+        else:
+            sign_test.append("")
+                
+    results = np.array([ str(coefs[1]) + sign_test[1], "(" + str(tvals[1]) + ")", \
+                         str(coefs[2]) + sign_test[2], "(" + str(tvals[2]) + ")", \
+                         str(coefs[0]) + sign_test[0], "(" + str(tvals[0]) + ")", r_squared])        
+    
+    resultsDf = pd.DataFrame()
+    if j == 0:
+        resultsDf["Lag = " + str(lag) + " day"] = legend
+        resultsDf[ticker] = results
+        allresDf = resultsDf
+    else:
+        resultsDf[ticker] = results
+     
+    if j > 0:
+        allresDf = pd.concat((allresDf, resultsDf), axis = 1)
+
    
-        X      = X[0:-lag, :]       #Lag matrix accordingly 
-        X      = sm.add_constant(X) #add constant
+    #######################################
+    ######### Control Regression ##########
     
-        reg       = sm.OLS(y, X).fit()
-        coefs     = np.round(reg.params*100, decimals = 3) #Multiply coefs by 100 to get in bps format
-        tvals     = np.round(reg.tvalues, decimals = 3)
-        pvals     = np.round(reg.pvalues, decimals = 3)
-        r_squared = np.round(reg.rsquared, decimals = 3)
-            
-        legend  = np.array(['\textbf{' +name + '}', "coefficient", "t stats", "p-values"])
-        alpha   = np.array([" ", coefs[0], tvals[0], pvals[0]])
-        b1      = np.array([" ", coefs[1], tvals[1], pvals[1]])
-        b2      = np.array([" ", coefs[2], tvals[2], pvals[2]])
-        r       = np.array([" ", r_squared, " ", " ", ])
-        lag_df = pd.DataFrame()
+    y_control = y_control[1:]
+    X_control = X_control[0:-lag, :]       #Lag matrix accordingly 
+    X_control = sm.add_constant(X_control) #add constant
+
+    reg_control       = sm.OLS(y_control, X_control).fit()
+    #netGamma_coef     = np.roun(reg_control.params[1]*100, decimals = 3) 
+    coefs_control     = np.round(reg_control.params*100, decimals = 3)   #Multiply net gamma coef by 100 to get bps format
+    tvals_control     = np.round(reg_control.tvalues, decimals = 3)
+    pvals_control     = np.round(reg_control.pvalues, decimals = 3)
+    r_squared_control = np.round(reg_control.rsquared, decimals = 3)
         
+    ### Alternative Result Print
+    legend_control = np.array(['$netGamma_{t-' + str(lag) + '}$', " ", '$IVOL_{t-1}$', " ", '$IVOL_{t-2}$', " ", '$IVOL_{t-3}$', " ", \
+                       '$|R_{t-1}|$', " ", '$|R_{t-2}|$', " ", '$|R_{t-3}|$', " ",  'Intercept', " ", '$R^2$' ])
         
-        #LASSO FOR Parameter selection
-        LassoFeature = LassoCV(n_alphas = 100, fit_intercept = False, normalize = False, cv = 10, \
-                              max_iter = 10000, tol = 1e-3)
-        LassoFeature.fit(X, y)
-        LassoCoefs = LassoFeature.coef_
-        isImportantFeatures = (LassoCoefs != 0) 
-        
-        
-        if i == 0: #First Data Frame
-            lag_df["Lag = " + str(lag) + " day(s)"]  = legend
-            lag_df[r'$\alpha$']                 = alpha
-            lag_df['$netGamma_t$']             = b1
-            lag_df['$IVOL_t$']                 = b2
-            lag_df['$R^2$']                    = r
+    sign_test_control = []
+    for pval in pvals_control:
+        if pval < 0.01:
+            sign_test_control.append("***")
+        elif pval < 0.05:
+            sign_test_control.append("**")
+        elif pval < 0.1:
+            sign_test_control.append("*")
         else:
-            lag_df["alpha (Lag = " + str(lag) + ")"]    = alpha
-            lag_df["netGamma (Lag = " + str(lag) + ")"] = b1 
-            lag_df["IVOL (Lag = " + str(lag) + ")"]     = b2 
-        
-        regResults.append(lag_df)
-        print(lag_df.to_latex(index=False, escape = False)) #print to latex
+            sign_test_control.append("")
 
+    results_control = np.array([ str(coefs_control[1]) + sign_test_control[1], "(" + str(tvals_control[1]) + ")", \
+                                 str(coefs_control[2]) + sign_test_control[2], "(" + str(tvals_control[2]) + ")", \
+                                 str(coefs_control[3]) + sign_test_control[3], "(" + str(tvals_control[3]) + ")", \
+                                 str(coefs_control[4]) + sign_test_control[4], "(" + str(tvals_control[4]) + ")", \
+                                 str(coefs_control[5]) + sign_test_control[5], "(" + str(tvals_control[5]) + ")", \
+                                 str(coefs_control[6]) + sign_test_control[6], "(" + str(tvals_control[6]) + ")", \
+                                 str(coefs_control[7]) + sign_test_control[7], "(" + str(tvals_control[7]) + ")", \
+                                 str(coefs_control[0]) + sign_test_control[0], "(" + str(tvals_control[0]) + ")", r_squared])        
         
-
-        ### Alternative Result Print
-        legend = np.array(['$netGamma_t$', " ", '$IVOL_t$', " ", 'Intercept', " ", '$R^2$' ])
         
-        sign_test = []
-        for pval in pvals:
-            if pval < 0.01:
-                sign_test.append("***")
-            elif pval < 0.05:
-                sign_test.append("**")
-            elif pval < 0.1:
-                sign_test.append("*")
-            else:
-                sign_test.append("")
+    results_controlDf = pd.DataFrame()
+    if j == 0:
+        results_controlDf["Lag = " + str(lag) + " day"] = legend_control
+        results_controlDf[ticker] = results_control
+        allres_controlDf = results_controlDf
+    else:
+        results_controlDf[ticker] = results_control
+   
         
-            
-        results = np.array([ str(coefs[1]) + sign_test[1], "(" + str(tvals[1]) + ")", \
-                             str(coefs[2]/100) + sign_test[2], "(" + str(tvals[2]) + ")", \
-                             str(coefs[0]/100) + sign_test[0], "(" + str(tvals[0]) + ")", r_squared])        
+    if j > 0:
+        allres_controlDf = pd.concat((allres_controlDf, results_controlDf), axis = 1)
         
-        resultsDf = pd.DataFrame()
-        if j == 0:
-            resultsDf["Lag = " + str(lag) + " day"] = legend
-            resultsDf[ticker] = results
-            allresDf = resultsDf
-        else:
-            resultsDf[ticker] = results
-       
-            
-        if j > 0:
-            allresDf = pd.concat((allresDf, resultsDf), axis = 1)
-
-
-        ##### Control Variable Regression
-        y_control = y_control[1:]
-        X_control = X_control[0:-lag, :]       #Lag matrix accordingly 
-        X_control = sm.add_constant(X_control) #add constant
     
-        reg_control       = sm.OLS(y_control, X_control).fit()
-        coefs_control     = np.round(reg_control.params, decimals = 3) #Multiply coefs by 100 to get in bps format
-        tvals_control     = np.round(reg_control.tvalues, decimals = 3)
-        pvals_control     = np.round(reg_control.pvalues, decimals = 3)
-        r_squared_control = np.round(reg_control.rsquared, decimals = 3)
-            
-        ### Alternative Result Print
-        legend_control = np.array(['$netGamma_t$', " ", '$IVOL_t$', " ", '$IVOL_{t-1}$', " ", '$IVOL_{t-2}$', " ", \
-                           '$|R_{t-1}|$', " ", '$|R_{t-2}|$', " ", '$|R_{t-3}|$', " ",  'Intercept', " ", '$R^2$' ])
-        
-        sign_test_control = []
-        for pval in pvals_control:
-            if pval < 0.01:
-                sign_test_control.append("***")
-            elif pval < 0.05:
-                sign_test_control.append("**")
-            elif pval < 0.1:
-                sign_test_control.append("*")
-            else:
-                sign_test_control.append("")
-
-        results_control = np.array([ str(coefs_control[1]) + sign_test_control[1], "(" + str(tvals_control[1]) + ")", \
-                                     str(coefs_control[2]) + sign_test_control[2], "(" + str(tvals_control[2]) + ")", \
-                                     str(coefs_control[3]) + sign_test_control[3], "(" + str(tvals_control[3]) + ")", \
-                                     str(coefs_control[4]) + sign_test_control[4], "(" + str(tvals_control[4]) + ")", \
-                                     str(coefs_control[5]) + sign_test_control[5], "(" + str(tvals_control[5]) + ")", \
-                                     str(coefs_control[6]) + sign_test_control[6], "(" + str(tvals_control[6]) + ")", \
-                                     str(coefs_control[7]) + sign_test_control[7], "(" + str(tvals_control[7]) + ")", \
-                                     str(coefs_control[0]) + sign_test_control[0], "(" + str(tvals_control[0]) + ")", r_squared])        
-        
-        
-        results_controlDf = pd.DataFrame()
-        if j == 0:
-            results_controlDf["Lag = " + str(lag) + " day"] = legend_control
-            results_controlDf[ticker] = results_control
-            allres_controlDf = results_controlDf
-        else:
-            results_controlDf[ticker] = results_control
-       
-            
-        if j > 0:
-            allres_controlDf = pd.concat((allres_controlDf, results_controlDf), axis = 1)
-            
-        
-        if scatter == True:
-            #x        = np.linspace(np.min(X[:, 1]), np.max(X[:, 1]), np.size(X[:, 1]))
-            #reg_line = coefs[0] + coefs[1] * x #+ coefs[2]*x**2
-            
-            fig, ax = plt.subplots()
-            
-            ax.scatter(X[:, 1], y, color = '#0504aa', s = 0.5)
-            #ax.plot(x, reg_line, color = "red", alpha = 0.5)
-            fig.suptitle("Absolute Returns vs Net Gamma Exposure (normalized)")
-            ax.set_xlabel("Net Gamma Exposure (Normalized)")
-            ax.set_ylabel("Daily Absolute Returns")
-            
-            textstr = '\n'.join((
-                'Asset = ' + ticker,
-                'Lag = ' + str(lag) + ' days',
-                r'$\alpha=%.5f$' % (coefs[0], ),
-                r'$\beta_1=%.5f$' % (coefs[1], ),
-                r'$\beta_2=%.5f$' % (coefs[2], ),
-                ))
-
-            # these are matplotlib.patch.Patch properties
-            props = dict(boxstyle='round', facecolor=prefColor, alpha=0.2)
-
-            # place a text box in upper left in axes coords
-            ax.text(0.65, 0.95, textstr, transform=ax.transAxes, fontsize=14, verticalalignment='top', bbox=props)
-            plt.show() 
-            
-            
-        residuals  = reg.resid
-        if hist == True: #Plot netGamma histogram
-            plot = plotResiduals(residuals, lag = lag, ticker = ticker)
     
+    
+    ########################################
+    ######### Liquidity Regression #########
+    
+    y_liq  = np.abs(xsret[lag:])
+    X_liq  = sm.add_constant(X_liq*1)
+    X_liq  = X_liq[0:-lag]
+    
+    reg_liq       = sm.OLS(y_liq, X_liq).fit()
+    coefs_liq     = np.round(reg_liq.params*100, decimals = 3)   #Multiply net gamma coef by 100 to get bps format
+    tvals_liq     = np.round(reg_liq.tvalues, decimals = 3)
+    pvals_liq     = np.round(reg_liq.pvalues, decimals = 3)
+    r_squared_liq = np.round(reg_liq.rsquared, decimals = 3)
+    
+    ### Alternative Result Print
+    legend_liq = np.array(['Negative $netGamma$', " ", 'Low Liquidity', " ", 'Negative $netGamma \times $ Low Liquidity', " ",  'Intercept', " ", '$R^2$' ])
+        
+    sign_test_liq = []
+    for pval in pvals_liq:
+        if pval < 0.01:
+            sign_test_liq.append("***")
+        elif pval < 0.05:
+            sign_test_liq.append("**")
+        elif pval < 0.1:
+            sign_test_liq.append("*")
+        else:
+            sign_test_liq.append("")
+
+    results_liq = np.array([ str(coefs_liq[1]) + sign_test_liq[1], "(" + str(tvals_liq[1]) + ")", \
+                                 str(coefs_liq[2]) + sign_test_liq[2], "(" + str(tvals_liq[2]) + ")", \
+                                 str(coefs_liq[3]) + sign_test_liq[3], "(" + str(tvals_liq[3]) + ")", \
+                                 str(coefs_liq[0]) + sign_test_liq[0], "(" + str(tvals_liq[0]) + ")", r_squared])        
+    #Store in DataFrame      
+    results_liqDf = pd.DataFrame()
+    if j == 0:
+        results_liqDf["Lag = " + str(lag) + " day"] = legend_liq
+        results_liqDf[ticker] = results_liq
+        allres_liqDf = results_liqDf
+    else:
+        results_liqDf[ticker] = results_liq
+           
+    if j > 0:
+        allres_liqDf = pd.concat((allres_liqDf, results_liqDf), axis = 1)
+    
+ 
+    
+    
+    
+    
+    if scatter == True:
+        #x        = np.linspace(np.min(X[:, 1]), np.max(X[:, 1]), np.size(X[:, 1]))
+        #reg_line = coefs[0] + coefs[1] * x #+ coefs[2]*x**2
+        
+        fig, ax = plt.subplots()
+        
+        ax.scatter(X[:, 1], y, color = '#0504aa', s = 0.5)
+        #ax.plot(x, reg_line, color = "red", alpha = 0.5)
+        fig.suptitle("Absolute Returns vs Net Gamma Exposure (normalized)")
+        ax.set_xlabel("Net Gamma Exposure (Normalized)")
+        ax.set_ylabel("Daily Absolute Returns")
+        
+        textstr = '\n'.join((
+            'Asset = ' + ticker,
+            'Lag = ' + str(lag) + ' days',
+            r'$\alpha=%.5f$' % (coefs[0], ),
+            r'$\beta_1=%.5f$' % (coefs[1], ),
+            r'$\beta_2=%.5f$' % (coefs[2], ),
+            ))
+
+        # these are matplotlib.patch.Patch properties
+        props = dict(boxstyle='round', facecolor=prefColor, alpha=0.2)
+
+        # place a text box in upper left in axes coords
+        ax.text(0.65, 0.95, textstr, transform=ax.transAxes, fontsize=14, verticalalignment='top', bbox=props)
+        plt.show() 
+        
+        
+    residuals  = reg.resid
+    if hist == True: #Plot netGamma histogram
+        plot = plotResiduals(residuals, lag = lag, ticker = ticker)
+
     #Concatenate
     #AssetDf = pd.concat(regResults, axis = 1)
     #print(AssetDf)
     
     #RegResultList.append(AssetDf)
+
+#Print To Latex
+print(allresDf.to_latex(index=False, escape = False)) 
+print(allres_controlDf.to_latex(index=False, escape = False))     
+print(allres_liqDf.to_latex(index=False, escape = False))      
     
-    print(allresDf.to_latex(index=False, escape = False)) #print to latex
-    print(allres_controlDf.to_latex(index=False, escape = False)) #print to latex    
-   
-    
+
 # X   = netGamma[0:-lag]
 # X   = sm.add_constant(X)
 # y   = np.abs(Returns[lag:])
